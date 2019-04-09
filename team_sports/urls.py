2954326da60f14f183b79738067a8755526d5286
Jiabin CAO <zy22023@nottingham.edu.cn>
2019-04-10 02:16:27 +0800

    Hide admin (Another branch may wrong)

--- team_sports/urls.py
+++ team_sports/urls.py
@@ -3,10 +3,8 @@ from django.urls import path
 from django.conf.urls import include,url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('ZvRoo4fuB5bbVsZb3r63/', admin.site.urls),
     path('', include('team_sports_app.urls', namespace = 'team_sports_app')),
     path('users/', include('users.urls', namespace = 'users'))
- 
+
 ]
-
-
